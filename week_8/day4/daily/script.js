bc4fd4c4119383759dbc2e9c7ca6bfb136d84370
pamenov <pamenov@yandex.ru>
2023-05-24 14:55:54 +0300

    week8_day4 daily

---
+++ week_8/day4/daily/script.js
@@ -0,0 +1,29 @@
+// In this exercise, you will use object oriented programming concepts to define and use a custom object in JavaScript.
+
+// Create a class named Video. The class should be constructed with the following parameters:
+// title (a string)
+// uploader (a string, the person who uploaded it)
+// time (a number, the duration of the video - in seconds)
+
+class Video {
+    constructor(title, uploader, time) {
+        this.title = title
+        this.uploader = uploader
+        this.time = time
+    }
+// Create a method called watch() which displays a string as follows:
+// “uploader parameter watched all time parameter of title parameter!”
+    watch() {
+        console.log(`${this.uploader} watched all ${this.time} min of ${this.title}!`)
+    }
+}
+// Instantiate a new Video instance and call the watch() method.
+const deepThroat = new Video("Deep Throat", "Mark Zuckerberg", 120)
+deepThroat.watch()
+// Instantiate a second Video instance with different values.
+
+const oopsIDidItAgain = new Video("Ooops I Did It again", "Britney Spears", 5)
+oopsIDidItAgain.watch()
+// Bonus: Use an array to store data for five Video instances (ie. title, uploader, time)
+// Think of the best data structure to save this information within the array.
+// Bonus: Loop through the array to instantiate those instances.

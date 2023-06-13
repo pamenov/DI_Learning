632305cd61ca222f1c7d4a15b2787115bb00642f
pamenov <pamenov@yandex.ru>
2023-06-13 15:54:13 +0300

    week11_day1 daily

---
+++ week_11/day1/daily/carousel/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import DemoCarousel from './Carousel'
+
+function App() {
+  return (
+    <div className="App">
+      < DemoCarousel />
+    </div>
+  );
+}
+
+export default App;
